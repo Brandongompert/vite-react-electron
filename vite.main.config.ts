e4bab7787bd649620a10cfbae2ce0b1d1a85c388
Brandon Gompert <brandongompert@gmail.com>
2023-12-20 14:03:00 -0500

    feat: add database import alias

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
 export default defineConfig({
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
-    mainFields: ['module', 'jsnext:main', 'jsnext'],
+    mainFields: ["module", "jsnext:main", "jsnext"],
+    alias: {
+      "@hooks": "/src/hooks/",
+      "@utils": "/src/utils/",
+      "@database": "/src/database/",
+      "@api": "/src/api/",
+    },
   },
 });
