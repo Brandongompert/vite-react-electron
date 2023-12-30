e6d9d3b8ed6f493215eae78118aece5878256549
Brandon Gompert <brandongompert@gmail.com>
2023-12-30 00:36:13 -0500

    feat: add react-query & update home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,30 @@
-import useCreateUser from "@hooks/useCreateUser";
+import useCreateUser from "@hooks/User/useCreateUser";
+import useDeleteUserById from "@hooks/User/useDeleteUserById";
+import useGetUserById from "@hooks/User/useGetUserById";
+import useUpdateUser from "@hooks/User/useUpdateUser";
 
 function Home() {
+  // Call the useApi hook with method set to 'delete'
+  const handleDeleteClick = useDeleteUserById(1);
+  const {
+    data: Bob,
+    loading: isBobLoading,
+    error: isBobError,
+  } = useGetUserById(1);
+
   return (
     // cool native html elements
     <>
-      <button onClick={() => useCreateUser({ name: "Billy" })}>
+      <button onClick={() => useCreateUser({ name: "Yolanda" })}>
         Create User
       </button>
+      <button onClick={() => useUpdateUser({ id: 1, name: "Not lance" })}>
+        Update User
+      </button>
+      <button onClick={() => console.log(Bob, isBobError, isBobLoading)}>
+        Get User
+      </button>
+      <button onClick={() => handleDeleteClick}>Delete User</button>
       <details>
         <summary>Click to toggle content</summary>
         <p>
